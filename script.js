82d5509e29f25bf460b759f576cac6dc0b539915
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-06-17 01:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function calculateMinCost() {
   //your code here
+
+	let text = document.getElementById("rope-lengths");
+
+	let arr=text.split(",");
+      for(let i in arr){
+        arr[i]=Number(arr[i]);
+      }
   
   
+      let sum=0;
   
+      function set(arr) {
+        console.log(arr);
+        arr.sort((a, b) => b - a);
+        let s = arr.pop() + arr.pop();
+        console.log(s,arr);
+        sum += s;
+        arr.push(s);
+      }
+  
+      while (arr.length>1) {
+          set(arr);
+      }
+
+	let res = document.getElementById("result");
+      console.log(res);
+      const textNode = document.createTextNode(sum);
+        res.appendChild(textNode);
 }  
